37a42a0a44bc4e22d895df9e6ea21782071dedc3
HashiCorp Training <training@hashicorp.com>
2017-06-30 13:49:44 +0000

    initial

---
+++ main.tf
@@ -0,0 +1,68 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-58475f3e
+#
+# Your subnet ID is:
+#
+#     subnet-04e4095f
+#
+# Your security group ID is:
+#
+#     sg-c326e1bb
+#
+# Your Identity is:
+#
+#     amazic-moose
+#
+
+terraform {
+  backend "atlas" {
+   name = "geramazic/training"
+  }
+}
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  type    = "string"
+  default = "eu-west-1"
+}
+variable "instances_count" {
+  default = "2"
+}
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-58475f3e"
+  count                  = "${var.instances_count}"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-04e4095f"
+  vpc_security_group_ids = ["sg-c326e1bb"]
+
+  tags = {
+    Identity = "amazic-moose"
+    Name     = "web ${count.index+1}/${var.instances_count}"
+  }
+}
+
+#module "example" {
+#  source = "./example-module"
+#  command = "echo yadayada"
+#}
+#
+output "public_dns" {
+  value = [ "${aws_instance.web.*.public_dns}" ]
+}
